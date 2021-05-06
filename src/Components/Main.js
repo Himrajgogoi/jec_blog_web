b0e50751fc81b2387ef8be96f644b07b45a9a81e
RG-404 <55778353+RG-404@users.noreply.github.com>
2021-05-06 21:22:41 +0530

    update useToken call

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,35 +1,30 @@
-import React, {useEffect, useState} from 'react'
-import {HashRouter, Route, Switch} from "react-router-dom";
-import DashBoard from './DashBoard';
-import  Login from './Account/login';
-import Register from './Account/register';
+import React, { useEffect, useState } from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import DashBoard from "./DashBoard";
+import Login from "./Account/login";
+import Register from "./Account/register";
 import Allarticles from "./AllArticles";
-import PersonalArticles from './PersonalArticles';
-import Profile from './Profile';
-import useToken from  "./common/useToken";
+import PersonalArticles from "./PersonalArticles";
+import Profile from "./Profile";
+import useToken from "./common/useToken";
 
 export default function Main() {
-
-     const {token, setToken} = useToken();
-
-     useEffect(function(){
-       console.log("token state changes");
-       console.log(token);
-     },[token])
      
-     if(!token){
-          return (<Login setToken={setToken}/>);
-     }
-     return(<div>
-          <HashRouter>
-                    <Switch>
-                        <Route path = "/" component={DashBoard}/>
-                            <Route exact path = "/login" component={Login}/>
-                            <Route exact path = "/register" component={Register}/>
-                            <Route path = "/all" component={Allarticles}/>
-                            <Route path = "/personal" component = {PersonalArticles}/>
-                            <Route path="/profile" component = {Profile}/>
-                    </Switch>
-            </HashRouter>
-     </div>);
+  const [token, setToken] = useToken("");
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+  return (
+    <HashRouter>
+      <Switch>
+        <Route path="/" component={DashBoard} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route path="/all" component={Allarticles} />
+        <Route path="/personal" component={PersonalArticles} />
+        <Route path="/profile" component={Profile} />
+      </Switch>
+    </HashRouter>
+  );
 }
